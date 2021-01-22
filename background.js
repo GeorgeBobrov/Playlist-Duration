b9d4f54c139727fb9cc4d480869fc35b5c1300ef
George Bobrov <skivvvvy@gmail.com>
2021-01-22 20:23:27 +0200

    Fixed bug with unspecified tabId

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.title)
     if (tab.url.startsWith("https://www.youtube.com/playlist") && (tab.status === "complete")) {
         console.log('run PlaylistDuration.js on ' + tab.url);
-        chrome.tabs.executeScript(null, {file:"PlaylistDuration.js"});
+        chrome.tabs.executeScript(tabId, {file:"PlaylistDuration.js"});
     }
 
 });
